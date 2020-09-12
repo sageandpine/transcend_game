44de6839b6436c14a4535e5676d597c13fa924ca
GoodTimesImmort <andy@immortalbrewco.com>
2020-09-12 15:16:32 -0700

    couple changes to make it functional with print statements

--- gt_inventory_idea001.py
+++ gt_inventory_idea001.py
@@ -1,27 +1,28 @@
+from sys import exit
 
-
-def inventory(fleshpack, full_fleshpack, soulpack, full_soulpack):
+def inventory_full(full_fleshpack, full_soulpack):
     """
-gt: inventory is composed of a function where each pack has its own list and
-also a variable that holds the value of True if the pack is full by default.
-This allows a better flow for the program. 
+gt: function where each pack has its own list and also a variable that holds
+the value of True if the pack is full by default. 
     """
     if full_fleshpack and full_soulpack:
         print("Inventory is full")
     elif full_fleshpack and not full_soulpack:
         print("fleshpack is full, would you like to access soulpack?")
-        #insert module to get input
+        #could go to soulpack directly or ask if you want to as prompted
+        soulpack()
     elif not full_fleshpack and full_soulpack:
-        print("soupack is full, would you like to access fleshpack?")
-        #insert module to get input 
+        print("soulpack is full, would you like to access fleshpack?")
+        #could go to fleshpack directly or ask if you want to as prompted
+        fleshpack()
     elif not full_fleshpack and not full_soulpack:
-        open_inventory()
-        #calls the function that user can interact with. 
+        display_inventory()
+        #calls the function that user can interact with.
 
-def open_inventory():
+def display_inventory():
     """
-gt: open inventory I haven't figured out yet, but it would take the input
-from user and maybe print the inventory. 
+a compact menu that forces the user to pick a pack to access before adding
+or removing items. 
     """
     print("INVENTORY MENU")
     print("#########################")
@@ -29,49 +30,50 @@ from user and maybe print the inventory.
     print("s = soulpack")
     print("c = close inventory")
     print("#########################")
+    inventory_input = input("please select one: ")
+    open_packs(inventory_input)
 
-##    if inventory_input="c":
-##        print("Exiting inventory")
-##    elif inventory_input="f":
-##        print("Accessing fleshpack")
-##        fleshpack()
-##    elif inventory_input="s":
-##        print("Accessing soulpack")
-##        soulpack()
-##    else:
-##        print("Input not valid, try again")
+def open_packs(inventory_input):
+    if inventory_input=="c":
+        print("Let's get back to the game!")
+    elif inventory_input=="f":
+        print("Accessing fleshpack")
+        fleshpack()
+    elif inventory_input=="s":
+        print("Accessing soulpack")
+        soulpack()
+    else:
+        print("Input not valid, try again")
+        display_inventory()
 
-    
+
 
 def fleshpack():
     print("FLESHPACK ACTIVATE")
     """
 gt: for both fleshpack and soulpack, I'm thinking list and strings to be able
-to slice and pull parts as needed. Using split. could help locate variables in
-a string that call the correct print out from a list.
+to slice and pull parts as needed. Using split. to know the position of the
+items. 
     """
-    
+    exit
 
 def soulpack():
     print("SOULPACK ACTIVATE")
     """
 gt: see above
     """
-fitem1="fitem1"
-fitem2="fitem2"
-fitem3="fitem3"
-sitem1="sitem1"
-sitem2="sitem2"
-sitem3="sitem3"
-fleshpack=[fitem1, fitem2, fitem3]
-soulpack=[sitem1, sitem2, sitem3]
+    exit
+
+
+
+
 
 #    """
 #gt: the idea here is that a player function could theoretically return a call
 #to inventory() with the correct set of conditions. You can test them by
 #changing the value of the Booleans. 
 #    """
-inventory(fleshpack, False, soulpack, False)
+inventory_full(False, False)
 
 
 
