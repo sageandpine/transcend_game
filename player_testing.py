4edc342dfa2bf8af788bc1a9be01134a678c7597
sageandpine <jeffgrindley702@gmail.com>
2020-09-15 13:09:05 -0700

    revisions 9/15

--- player_testing.py
+++ player_testing.py
@@ -1,116 +1,153 @@
+from sys import exit
+
 class Player(object):
 
     def __init__(self, name):
 
-        inventory = []
+        flesh_pack = []
+        soul_pack = []
+        while len(flesh_pack) == 0:
+            full_flesh_pack = False
 
-        map_counter = 0
-        player_counter = 0
-        inventory_counter = 0
+        while len(soul_pack) == 0:
+            full_soul_pack = False
 
+        self.full_soul_pack = full_soul_pack
+        self.full_flesh_pack = full_flesh_pack
         self.name = name
-        self.map_counter = map_counter
-        self.player_counter = player_counter
-        self.inventory_counter = inventory_counter
-        self.inventory = inventory
+        self.flesh_pack = flesh_pack
+        self.soul_pack = soul_pack
         
-    def open_mind(self):
-        print(self.name + " has opened their mind and is in the GAME!")
+    def inventory_full(self):
+        """
+        gt: function where each pack has its own list and also a variable that holds
+        the value of True if the pack is full by default. 
+        """
 
-    def status(self):
-        print("Current Status is--- ")
-        print("Map = " + str(self.map_counter))
-        print("Inventory = " + str(self.inventory_counter))
-        print("Self Worth = " + str(self.player_counter))
+        if full_fleshpack == True and full_soul_pack == True:
+            print("Inventory is full")
 
-    def show_inventory(self):
-        print(f"Inventory - {self.inventory}")
+        elif full_fleshpack == True and full_soulpack== False:
+            print("fleshpack is full")
+            #could go to soulpack directly or ask if you want to as prompted
+            self.display_inventory()
 
-    def add_inv_num(self, num):
-        self.inventory_counter += num
-        print("cha-ching!")
-        
-    def rm_inv_num(self, num):
-        self.inventory_counter -= num
-        print("depleted!")
-
-    #def add_map_num(self, num):
-     #   self.map_counter += num
-      #  print("cha-ching!")
-        
-    #def rm_map_num(self, num):
-     #   self.map_counter -= num
-      #  print("depleted!")
-
-
-    def rmv_item(self):
-        print("What item would you like to remove? ")
-        print(self.inventory)
-
-        item = input("> ")
-
-        print(f"You've removed {item} from inventory!")
-        self.inventory.remove(item)
-        self.rm_inv_num(1)
-        
-        print("Would you like to remove another item? Y/N")
-
-        choice = input(">  ")
-        
-        if "Y" in choice and len(self.inventory) != 0 or "y" in choice and len(self.inventory) != 0:
-            self.rmv_item()
-
-    
-        elif "Y" in choice and len(self.inventory) == 0 or "y" in choice and len(self.inventory) == 0:
-            print("Inventory empty!")
-            self.add_item()        
+        elif full_fleshpack == False and full_soulpack == True:
+            print("soulpack is full")
+            #could go to fleshpack directly or ask if you want to as prompted
+            self.display_inventory()
 
         else:
-            print("Let's get back to the game!")
-            #exit()
-        
-    def add_item(self):
-        print("Would you like to add (A), remove (R) an item or return to the game (any other key)? ")
+            print("zzzzzz")
+            self.display_inventory()
+            #calls the function that user can interact with.
 
-        choice = input(">  ")
 
-        if "A" in choice and len(self.inventory) != 3 or "a" in choice and len(self.inventory) != 3:
-            print("What item would you like to add? ")
+
+    def rmv_flesh(self):
+        if self.flesh_pack == 0:
+            print("you can't squeeze blood from a stone...")
+            print("inventory empty")
+            display_inventory()
+        else:
+            print("What item would you like to remove? ")
+            print(self.flesh_pack)
+
             item = input("> ")
 
-            self.inventory.append(item)
-            self.add_inv_num(1)
+            print(f"You've removed {item} from inventory!")
+            self.flesh_pack.remove(item)
+            self.display_inventory()
+
+    def rmv_soul(self):
+        if self.soul == 0:
+            print("your soul is empty...")
+            display_inventory()
+            
+        else:
+            print("What item would you like to remove? ")
+            print(self.soul_pack)
+
+            item = input("> ")
+
+            print(f"You've removed {item} from inventory!")
+            self.soul_pack.remove(item)
+            self.display_inventory()
+            
+#while len*(inventory) != 3 return True etc. 
+
+    def display_inventory(self):
+        """
+        a compact menu that forces the user to pick
+        a pack to access before adding
+        or removing items. 
+        """
+        
+    print("INVENTORY MENU")
+    print("#########################")
+    print("f = fleshpack")
+    print("s = soulpack")
+    print("c = close inventory")
+    print("#########################")
+    choice = input("please select one: ")
+
+    if "c" in choice:
+        print("returning to game")
+
+    elif "f" in choice and len(self.flesh_pack) != 3:
+        print(f"Fleshpack-- {self.flesh_pack}")
+        print(
+        self.add_item()
+
+    elif "f" in choice and len(self.flesh_pack) == 3:
+        self.inventory_full()
+
+    elif "s" in choice and len(self.soul_pack) != 3:
+        print(f"Soulpack-- {self.soul_pack}")
+        self.add_item()
+
+    else:
+        print("not sure what is going on!")
+        
+   # open_packs(choice)
+    def add_soul(self):
+        if len(self.soul_pack) == 3:
+            self.inventory_full()
+        
+        else:
+            print("What item would you like to add? ")
+            item = input("> ")
+       
+            self.soul_pack.append(item)
             
             print(f"You've added {item} to inventory!")
-            self.add_item()
 
-            print("Return to game (G) or Edit (E) inventory?")
-            choice = input("> ")
-            if "G" in choice or "g" in choice:
-                print("pronto toronto!")
-
-            elif "E" in choice or "e" in choice:
-                self.add_item()
-
+            if len(self.soul_pack) == 3:
+                self.inventory_full()
+                
             else:
-                print("does not compute, but i make my own decisions sometimes....")
-                self.add_item()
-
-        elif "A" in choice and len(self.inventory) == 3 or "a" in choice and len(self.inventory) == 3:
-            print("You can't do that! Inventory full!")
-            self.add_item()
-
-        elif "R" in choice or "r" in choice:
-            self.rmv_item()
-            
-
+                self.display_inventory()
+                
+    def add_flesh(self):
+        if len(self.flesh_pack) == 3:
+            self.inventory_full()
+        
         else:
-            print("Let's get back to the game!")
-            #exit()    
+            print("What item would you like to add? ")
+            item = input("> ")
+       
+            self.flesh_pack.append(item)
+            
+            print(f"You've added {item} to flesh pack!")
+
+            if len(self.flesh_pack) == 3:
+                self.inventory_full()
+                
+            else:
+                self.display_inventory()
+                
+        
         
 ted = Player("Ted")
-ted.open_mind()
-ted.status()
-ted.show_inventory()
-ted.add_item()
-ted.status()
+ted.display_inventory()
+
