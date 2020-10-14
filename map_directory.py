40222abc079b2163bc17534f7b58bc97df792314
sageandpine <jeffgrindley702@gmail.com>
2020-10-14 11:36:12 -0700

    fixed recent errors in functions. still need option for user to remove items. functions are there, the connections are not.

--- map_directory.py
+++ map_directory.py
@@ -5,38 +5,47 @@ Created on Mon Sep  7 22:32:37 2020
 
 @author: goodtimes
 """
-scene1 = "Let's see if this works 1"
-scene2 = "Let's see if this works 2"
-scene3 = "Let's see if this works 3"
-scene4 = "Let's see if this works 4"
-scene5 = "Let's see if this works 5"
+#scene1 = "Let's see if this works 1"
+#scene2 = "Let's see if this works 2"
+#scene3 = "Let's see if this works 3"
+#scene4 = "Let's see if this works 4"
+#scene5 = "Let's see if this works 5"
+
+
+
+
+
+
+class Scene(object):
+
+    def enter(self):
+
+
+
+
+
 
 
 class Map(object):
-    
-    def __init__(self,scene):
-
-        map_directory = {
-                        "scene1" : scene1,
+     scenes = {
+                        "Death" : Death(),
                         "scene2" : scene2,
                         "scene3" : scene3,
                         "scene4" : scene4,
                         "scene5" : scene5            
                         }
-
-        
-        self.map_directory = map_directory
-        self.scene = scene
+     
+    def __init__(self,start_scene):
+        self.start_scene = start_scene
 
 
     def next_scene(self):
-        val = scene.get(scene_name)
+        val = Map.scenes.get(scene_name)
         return val
 
     def opening_scene(self):
         print(f"{self.scene}")
-        print(f"{self.map_directory['scene4']}")
-        return self.scene
+        return self.next_scene(self.start_scene)
     
 
 m1 = Map(scene1)
